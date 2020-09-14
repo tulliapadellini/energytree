974028465be8409eccff015ee0c0bda2e7663db7
Riccardo Giubilei <riccardo.giubilei@uniroma1.it>
2020-09-14 20:09:30 +0200

    S2 update
    added power

--- S2_ctree+mixed.R
+++ S2_ctree+mixed.R
@@ -13,6 +13,7 @@ library(igraph)
 library(NetworkDistance)
 library(ggparty)
 library(checkmate)
+library(pbapply)
 
 # Functions
 source("functions_v2.R")
@@ -22,6 +23,7 @@ source("party_v2.R")
 source("plot_v2.R")
 source("ggparty_v1.R")
 source("get_plot_data_v1.R")
+source('etree_sel.R') #starts to fit an etree, but only return the first var's id
 
 
 # Data simulation -------------------------------------------------------------
@@ -30,7 +32,7 @@ source("get_plot_data_v1.R")
 n_obs <- 100
 
 # Number of simulations
-n_sim <- 10000
+n_sim <- 1000
 
 # Mean of the normal distribution by which obs of the second class are modelled
 mu = 0
@@ -56,18 +58,18 @@ for(i in 1:n_sim){
   # Graph: Erdos-Renyi (1959) model with same connection prob
   x5 <- lapply(1:n_obs, function(j) igraph::sample_gnp(100, 0.2))
   # Graph: Small-World model (Watts-Strogatz, 1998) with same rewiring prob
-  x6 <- list(lapply(1:n_obs, function(j) igraph::sample_smallworld(1, 100, 5, 0.1)))
+  #x6 <- list(lapply(1:n_obs, function(j) igraph::sample_smallworld(1, 100, 5, 0.1)))
   # Graph: Preferential Attachment model (Barabasi-Albert, 1999)
-  x7 <- list(lapply(1:n_obs, function(j) igraph::sample_pa(100)))
+  #x7 <- list(lapply(1:n_obs, function(j) igraph::sample_pa(100)))
   # Functional: Ornstein-Uhlenbeck random process
   x8 <- fda.usc::rproc2fdata(n_obs, seq(0, 1, len = 20), sigma = 'OrnsteinUhlenbeck')
   # Functional: Wiener random process
-  x9 <- fda.usc::rproc2fdata(n_obs, seq(0, 1, len = 20), sigma = 'brownian')
+  #x9 <- fda.usc::rproc2fdata(n_obs, seq(0, 1, len = 20), sigma = 'brownian')
   # Covariates list for this simulation
   etree_sim_cov[[i]] <- list(x1 = x1, x2 = x2,
                          x3 = x3, x4 = x4,
-                         x5 = x5, x6 = x6, x7 = x7,
-                         x8 = x8, x9 = x9)
+                         x5 = x5, #x6 = x6, x7 = x7,
+                         x8 = x8)#, x9 = x9)
   print(i)
 }
 
@@ -82,27 +84,79 @@ for(i in 1:n_sim){
 
 # Variable selection probabilities (under independence) -----------------------
 
-# First variable selection
-source('etree_sel.R') #starts to fit an etree, but only return the first var's id
-library(pbapply)
-etree_first_var <- pbmapply(function(covs, resp){
-  set.seed(123)
-  etree_sel(response = resp,
-            covariates = covs,
-            case.weights = NULL,
-            minbucket = 1,
-            alpha = 1,
-            R = 1000,
-            split.type = 'cluster',
-            coef.split.type = 'test',
-            p.adjust.method = 'fdr')
-},
-etree_sim_cov,
-etree_sim_resp,
-SIMPLIFY = TRUE)
+# First-variable selection
+etree_first_var <- pbmapply(resp = etree_sim_resp,
+                            covs = etree_sim_cov,
+                            function(resp, covs){
+                              set.seed(123)
+                              etree_sel(response = resp,
+                                        covariates = covs,
+                                        case.weights = NULL,
+                                        minbucket = 10,
+                                        alpha = 0.05,
+                                        R = 1000,
+                                        split.type = 'cluster',
+                                        coef.split.type = 'test',
+                                        p.adjust.method = 'fdr')
+                            },
+                            SIMPLIFY = TRUE)
 
 # Frequency plot
 barplot(table(etree_first_var))
 
 # Proportions
 prop.table(table(etree_first_var))
+
+
+# Power and conditional probability (growing association) ---------------------
+
+# Covariates: keep the first three for numeric and only one for each structured
+etree_sim_cov2 <- lapply(etree_sim_cov,
+                         function(i_sim) i_sim[c('x1', 'x2', 'x3', 'x5', 'x8')])
+
+# Grid values for mu
+mu_grid = seq(0, 1, 0.05)
+
+# etree fit: n_sim simulations for each value of mu
+etree_mu_grid <- lapply(mu_grid,
+                        function(mu) {
+                          set.seed(123)
+                          #generate response based on mu
+                          etree_sim_resp2 <- list()
+                          for(i in 1:n_sim){
+                            set.seed(i)
+                            etree_sim_resp2[[i]] <- c(rnorm(n_obs/2, mean = 0,
+                                                            sd = 1),
+                                                      rnorm(n_obs/2, mean = mu,
+                                                            sd = 1))
+                          }
+                          #first-variable selection
+                          thismu_varsel <- pbmapply(resp = etree_sim_resp2,
+                                                    covs = etree_sim_cov2,
+                                                    function(resp, covs){
+                                                      #first-variable selection
+                                                      etree_sel(response = resp,
+                                                                covariates = covs,
+                                                                case.weights = NULL,
+                                                                minbucket = 10,
+                                                                alpha = 0.05,
+                                                                R = 1000,
+                                                                split.type = 'cluster',
+                                                                coef.split.type = 'test',
+                                                                p.adjust.method = 'fdr')
+                                                    },
+                                                    SIMPLIFY = TRUE)
+                          return(thismu_varsel)
+
+                        })
+
+
+# Power
+etree_power <- sapply(etree_mu_grid,
+                      function(thismu_varsel) {
+                        pow = 1 - sum(sapply(thismu_varsel, is.null)) / n_sim
+                        return(pow)
+                      })
+
+
+
