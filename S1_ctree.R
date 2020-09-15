9b5cd99c06bdb107e5d6d233f37c556d7449d836
Riccardo Giubilei <riccardo.giubilei@uniroma1.it>
2020-09-15 16:46:54 +0200

    S1 update & completed
    added power, conditional probability and plots

--- S1_ctree.R
+++ S1_ctree.R
@@ -12,6 +12,7 @@ library(igraph)
 library(NetworkDistance)
 library(ggparty)
 library(checkmate)
+library(pbapply)
 
 # Functions
 source("functions_v2.R")
@@ -21,6 +22,7 @@ source("party_v2.R")
 source("plot_v2.R")
 source("ggparty_v1.R")
 source("get_plot_data_v1.R")
+source('etree_sel.R') #starts to fit an etree, but only return the first var's id
 
 
 # Data simulation -------------------------------------------------------------
@@ -35,7 +37,7 @@ n_sim <- 10000
 mu = 0
 
 # Covariates simulations
-ctree_sim <- list()
+ctree_sim_cov <- list()
 for(i in 1:n_sim){
   set.seed(i)
   # Uniform between 0 and 1
@@ -67,42 +69,119 @@ for(i in 1:n_sim){
 }
 
 
-# Model fitting ---------------------------------------------------------------
-
-### Classification Energy Tree ###
-etree_sim <- mapply(function(covs, resp){
-  set.seed(123)
-  etree(response = resp,
-        covariates = covs,
-        case.weights = NULL,
-        minbucket = 1,
-        alpha = 0.05,
-        R = 1000,
-        split.type = 'cluster',
-        coef.split.type = 'test',
-        p.adjust.method = 'fdr')
-},
-ctree_sim,
-ctree_sim_resp)
-
-
-
 # Variable selection probabilities (under independence) -----------------------
 
-# First variable selection
-source('etree_sel.R') #starts to fit an etree, but only return the first var's id
-etree_first_var <- mapply(function(covs, resp){
-  set.seed(123)
-  etree_sel(response = resp,
-            covariates = covs,
-            case.weights = NULL,
-            minbucket = 1,
-            alpha = 1,
-            R = 1000,
-            split.type = 'cluster',
-            coef.split.type = 'test',
-            p.adjust.method = 'fdr')
-},
-ctree_sim,
-ctree_sim_resp,
-SIMPLIFY = TRUE)
+# First-variable selection
+ctree_first_var <- pbmapply(resp = ctree_sim_resp,
+                            covs = ctree_sim_cov,
+                            function(resp, covs){
+                              set.seed(123)
+                              etree_sel(response = resp,
+                                        covariates = covs,
+                                        case.weights = NULL,
+                                        minbucket = 10,
+                                        alpha = 0.05,
+                                        R = 1000,
+                                        split.type = 'cluster',
+                                        coef.split.type = 'test',
+                                        p.adjust.method = 'fdr')
+                            },
+                            SIMPLIFY = TRUE)
+
+# Frequency plot
+barplot(table(ctree_first_var))
+
+# Proportions
+prop.table(table(ctree_first_var))
+
+
+# Power and conditional probability (growing association) ---------------------
+
+# Grid values for mu
+mu_grid = seq(0, 1, 0.05)
+
+# etree fit: n_sim simulations for each value of mu
+ctree_mu_grid <- lapply(mu_grid,
+                        function(mu) {
+                          set.seed(123)
+                          #generate response based on mu
+                          ctree_sim_resp2 <- list()
+                          for(i in 1:n_sim){
+                            set.seed(i)
+                            ctree_sim_resp2[[i]] <- c(rnorm(n_obs/2, mean = 0,
+                                                            sd = 1),
+                                                      rnorm(n_obs/2, mean = mu,
+                                                            sd = 1))
+                          }
+                          #first-variable selection
+                          thismu_varsel <- pbmapply(resp = ctree_sim_resp2,
+                                                    covs = ctree_sim_cov,
+                                                    function(resp, covs){
+                                                      #first-variable selection
+                                                      etree_sel(response = resp,
+                                                                covariates = covs,
+                                                                case.weights = NULL,
+                                                                minbucket = 10,
+                                                                alpha = 0.05,
+                                                                R = 1000,
+                                                                split.type = 'coeff',
+                                                                coef.split.type = 'test',
+                                                                p.adjust.method = 'fdr')
+                                                    },
+                                                    SIMPLIFY = TRUE)
+                          return(thismu_varsel)
+
+                        })
+
+
+### Power ###
+
+# Power calculation
+ctree_power <- sapply(ctree_mu_grid,
+                      function(thismu_varsel) {
+                        #proportion of not-null values
+                        pow = 1 - sum(sapply(thismu_varsel, is.null)) / n_sim
+                        return(pow)
+                      })
+names(ctree_power) <- as.character(mu_grid)
+
+# Plot
+plot(y = ctree_power, x = names(ctree_power), ylab = 'Simulated Power',
+     xlab = expression(mu), main = 'Energy Trees', type = 'l')
+abline(h = 0.05, lty = 2)
+
+
+### Conditional probability ###
+
+# Index of x3, that is the discriminating covariate
+dis_var_idx <- as.integer(which(names(ctree_sim_cov[[1]]) == 'x6'))
+
+# Conditional probability calculation
+ctree_cp <- sapply(ctree_mu_grid,
+                   function(thismu_varsel) {
+                     #variables' id (only when a variable is selected)
+                     var_ifsel = unlist(thismu_varsel[!sapply(thismu_varsel,
+                                                              is.null)])
+                     #number of times a variable is selected
+                     n_var_ifsel = length(var_ifsel)
+                     #proportion of times the correct variable is selected
+                     cp = sum(var_ifsel == dis_var_idx) / n_var_ifsel
+                     return(cp)
+                   })
+names(ctree_cp) <- as.character(mu_grid)
+
+# Plot
+plot(y = ctree_cp, x = names(ctree_cp), ylab = 'Conditional Probability of Correct Split', xlab = expression(mu), main = 'Energy Trees', type = 'l')
+
+
+### Side-by-side plot ###
+old_par = par()
+par(mfrow = c(1, 2), pty = 's')
+#first plot: power
+plot(y = ctree_power, x = names(ctree_power), ylab = 'Simulated Power',
+     xlab = expression(mu), type = 'l')
+abline(h = 0.05, lty = 2)
+#second plot: conditional probability
+plot(y = ctree_cp, x = names(ctree_cp), ylab = 'Conditional Probability of Correct Split', xlab = expression(mu), type = 'l')
+par(old_par)
+
